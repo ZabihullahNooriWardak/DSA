e7b1bd1fba4dc4ce2f7738c455618df35696008b
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-10-30 19:15:41 +0430

    java challange

---
+++ JumpGame2InJava/jump.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    public int jump(int[] nums) {
+        System.out.println(0);
+        return 0;
+    }
+public static void main(String []args){
+    System.out.println("hellos");
+}
+}
