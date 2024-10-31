a02e0006f9d2169b7e518d678d9d5d119288242b
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-10-31 18:30:25 +0430

    trying to accomodate

--- JumpGame2InJava/jump.java
+++ JumpGame2InJava/jump.java
@@ -1,10 +1,33 @@
 package JumpGame2InJava;
 class Solution {
     public int jump(int[] nums) {
-        System.out.println(0);
+     int counter=0;
+     int changeable;
+     int BigItem = nums[0];
+     for(changeable =0; changeable<BigItem;){
+          if(BigItem >= nums.length-1 && BigItem == nums[0]){
+            return 1;
+          }
+     if(nums[changeable] > BigItem){
+        BigItem = nums[changeable];
+        changeable++;
+     }
+
+
+
+
+          
+
+     }
+
+
+
+
+
+
         return 0;
     }
 public static void main(String []args){
-    System.out.println("hellos");
+
 }
 }
